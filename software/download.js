15bf04bdf8530493d672d1a1be1261d6e6ffc227
Wohou <benjamin.cottone@epitech.eu>
2026-01-08 17:13:02 +0100

    [~] Forgot the change the name of the exe file for the download. mb

--- software/download.js
+++ software/download.js
@@ -1,6 +1,6 @@
 function downloadForPlatform(platform) {
     if (platform === 'windows') {
-        window.location.href = window.location.origin + '/files/LighTouch_Setup.exe';
+        window.location.href = window.location.origin + '/files/LighTouch.exe';
     } else if (platform === 'linux') {
         // Currently disabled
         alert('La version Linux est en cours de développement. Restez à l\'écoute !');
